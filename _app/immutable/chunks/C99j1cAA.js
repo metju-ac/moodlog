86ee328b67256c8361e5c7605c97f00075944a52
Matej Klima <matej.klima5@gmail.com>
2025-12-01 17:10:31 +0100

    Updates

--- _app/immutable/chunks/C99j1cAA.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_153ky43?.base??"/moodlog",a=globalThis.__sveltekit_153ky43?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/DXC-TJRL.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1qq68db?.base??"/moodlog",a=globalThis.__sveltekit_1qq68db?.assets??s??"";export{a,s as b};
