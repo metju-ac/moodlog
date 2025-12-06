bf831c7fdf8e652220bcd030300974850f5cf789
Peter Romancik <romancikpeter@gmail.com>
2025-12-06 16:14:26 +0100

    Updates

---
+++ _app/immutable/chunks/CSjZUAZp.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_q93q5t?.base??"/moodlog",t=globalThis.__sveltekit_q93q5t?.assets??s??"";export{t as a,s as b};
--- _app/immutable/chunks/CqY_ep6q.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_12w8omz?.base??"/moodlog",o=globalThis.__sveltekit_12w8omz?.assets??s??"";export{o as a,s as b};
