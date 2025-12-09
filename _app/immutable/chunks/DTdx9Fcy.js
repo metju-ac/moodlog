f4f8c1c834c18452b4a8c2494c4dab9e4f5de9cf
Ondřej Hložek <hlozek.o1@gmail.com>
2025-12-09 16:55:38 +0100

    Updates

--- _app/immutable/chunks/DTdx9Fcy.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1r6ucjb?.base??"/moodlog",a=globalThis.__sveltekit_1r6ucjb?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/uIsBiIe6.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_pjnec0?.base??"/moodlog",e=globalThis.__sveltekit_pjnec0?.assets??s??"";export{e as a,s as b};
