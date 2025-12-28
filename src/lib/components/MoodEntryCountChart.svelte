16ed2e671803b94cd060f984e64dc3682c9be6cf
Peter Romancik <romancikpeter@gmail.com>
2025-12-28 17:16:53 +0100

    feat(#63): make graph labels less prominent

--- src/lib/components/MoodEntryCountChart.svelte
+++ src/lib/components/MoodEntryCountChart.svelte
@@ -98,9 +98,9 @@
     <Card.Title class="text-2xl">
       Entry Count
       {#if groupingStrategy === 'week'}
-        (by week)
+        <span class="text-base text-muted-foreground">(by week)</span>
       {:else if groupingStrategy === 'month'}
-        (by month)
+        <span class="text-base text-muted-foreground">(by month)</span>
       {/if}
     </Card.Title>
   </Card.Header>
